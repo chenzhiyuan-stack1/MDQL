562aa787586b1bf828fa7f359e9a5618db23ec5e
Charlie <chenzhiyuan105@gmail.com>
2025-11-03 10:42:37 +0800

    414坏了

--- main_e2e.py
+++ main_e2e.py
@@ -43,7 +43,8 @@ hyperparameters = {
 # hyperparameters from v14_iql.py
 TensorBatch = List[torch.Tensor]
 # evaluation_dataset_path = '/home/czy/Schaferct/ALLdatasets/emulate'
-evaluation_dataset_path = '/home/min414/data1/Schaferct/ALLdatasets/1'
+# evaluation_dataset_path = '/home/min414/data1/Schaferct/ALLdatasets/1'
+evaluation_dataset_path = '/home/czy/offlineRL-rtc-bwp-2/rtc_srpo/ALLdatasets'
 ENUM = 20  # every 5 evaluation set
 # 拿出ENUM个测试集？
 small_evaluation_datasets = []
@@ -395,7 +396,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "--dataset_path",
         # default = "/home/czy/Schaferct/mstrain-id-123.pickle",
-        default="/home/min414/data1/Schaferct/training_dataset_pickle/v8.pickle",
+        # default="/home/min414/data1/Schaferct/training_dataset_pickle/v8.pickle",
+        # default="/home/czy/Schaferct/mstrain-id-345.pickle",
+        default='/home/czy/Schaferct/v8.pickle',
     )
     parser.add_argument(
         "--add_data_path",
